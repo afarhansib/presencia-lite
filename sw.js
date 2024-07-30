ce868f9e96ba9079ed65f0c0c7faccc64a3f91b3
Ahmad Farhan Sibghotalloh <afarhansib@gmail.com>
2024-07-30 23:02:09 +0700

    initial

---
+++ sw.js
@@ -0,0 +1,63 @@
+const CACHE_NAME = 'presencia-lite-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://unpkg.com/vue@3/dist/vue.global.js',
+  '/public/tailwindcss.min.js',
+  '/public/logo.svg'
+  // Add other assets you want to cache
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.includes('/version.json')) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(
+          (response) => {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                // Only cache if it's not a chrome-extension URL
+                if (!event.request.url.startsWith('chrome-extension://')) {
+                  cache.put(event.request, responseToCache);
+                }
+              });
+            return response;
+          }
+        );
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
